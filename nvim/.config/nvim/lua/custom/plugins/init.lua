9a7ec71f80486c19dd4e92f352f54e1ade520a77
Roasteg <krostislav468@gmail.com>
2025-03-10 19:59:06 +0200

    Add vue support, closing tags

--- nvim/.config/nvim/lua/custom/plugins/init.lua
+++ nvim/.config/nvim/lua/custom/plugins/init.lua
@@ -5,98 +5,120 @@
 -- See the kickstart.nvim README for more information
 --
 return {
-  {
-    'romgrk/barbar.nvim',
-    dependencies = {
-      'lewis6991/gitsigns.nvim',     -- OPTIONAL: for git status
-      'nvim-tree/nvim-web-devicons', -- OPTIONAL: for file icons
-    },
-    init = function()
-      local map = vim.api.nvim_set_keymap
-      local opts = { noremap = true, silent = true }
+	{
+		"romgrk/barbar.nvim",
+		dependencies = {
+			"lewis6991/gitsigns.nvim", -- OPTIONAL: for git status
+			"nvim-tree/nvim-web-devicons", -- OPTIONAL: for file icons
+		},
+		init = function()
+			local map = vim.api.nvim_set_keymap
+			local opts = { noremap = true, silent = true }
 
-      -- Move to previous/next
-      map('n', '<S-Tab>', '<Cmd>BufferPrevious<CR>', opts)
-      map('n', '<Tab>', '<Cmd>BufferNext<CR>', opts)
+			-- Move to previous/next
+			map("n", "<S-Tab>", "<Cmd>BufferPrevious<CR>", opts)
+			map("n", "<Tab>", "<Cmd>BufferNext<CR>", opts)
 
-      -- Re-order to previous/next
-      map('n', '<A-<>', '<Cmd>BufferMovePrevious<CR>', opts)
-      map('n', '<A->>', '<Cmd>BufferMoveNext<CR>', opts)
+			-- Re-order to previous/next
+			map("n", "<A-<>", "<Cmd>BufferMovePrevious<CR>", opts)
+			map("n", "<A->>", "<Cmd>BufferMoveNext<CR>", opts)
 
-      -- Goto buffer in position...
-      map('n', '<A-1>', '<Cmd>BufferGoto 1<CR>', opts)
-      map('n', '<A-2>', '<Cmd>BufferGoto 2<CR>', opts)
-      map('n', '<A-3>', '<Cmd>BufferGoto 3<CR>', opts)
-      map('n', '<A-4>', '<Cmd>BufferGoto 4<CR>', opts)
-      map('n', '<A-5>', '<Cmd>BufferGoto 5<CR>', opts)
-      map('n', '<A-6>', '<Cmd>BufferGoto 6<CR>', opts)
-      map('n', '<A-7>', '<Cmd>BufferGoto 7<CR>', opts)
-      map('n', '<A-8>', '<Cmd>BufferGoto 8<CR>', opts)
-      map('n', '<A-9>', '<Cmd>BufferGoto 9<CR>', opts)
-      map('n', '<A-0>', '<Cmd>BufferLast<CR>', opts)
-    end,
-    opts = {
-      animation = true,
-      sidebar_filetypes = {
-        ['neo-tree'] = true,
-      },
-      -- insert_at_start = true,
-      -- …etc.
-    },
-    version = '^1.0.0',
-  },
-  { 'habamax/vim-godot', event = 'VimEnter' },
-  {
-    'nvim-neo-tree/neo-tree.nvim',
-    branch = 'v3.x',
-    dependencies = {
-      'nvim-lua/plenary.nvim',
-      'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
-      'MunifTanjim/nui.nvim',
+			-- Goto buffer in position...
+			map("n", "<A-1>", "<Cmd>BufferGoto 1<CR>", opts)
+			map("n", "<A-2>", "<Cmd>BufferGoto 2<CR>", opts)
+			map("n", "<A-3>", "<Cmd>BufferGoto 3<CR>", opts)
+			map("n", "<A-4>", "<Cmd>BufferGoto 4<CR>", opts)
+			map("n", "<A-5>", "<Cmd>BufferGoto 5<CR>", opts)
+			map("n", "<A-6>", "<Cmd>BufferGoto 6<CR>", opts)
+			map("n", "<A-7>", "<Cmd>BufferGoto 7<CR>", opts)
+			map("n", "<A-8>", "<Cmd>BufferGoto 8<CR>", opts)
+			map("n", "<A-9>", "<Cmd>BufferGoto 9<CR>", opts)
+			map("n", "<A-0>", "<Cmd>BufferLast<CR>", opts)
+		end,
+		opts = {
+			animation = true,
+			sidebar_filetypes = {
+				["neo-tree"] = true,
+			},
+			-- insert_at_start = true,
+			-- …etc.
+		},
+		version = "^1.0.0",
+	},
+	{ "habamax/vim-godot", event = "VimEnter" },
+	{
+		"nvim-neo-tree/neo-tree.nvim",
+		branch = "v3.x",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+			"MunifTanjim/nui.nvim",
 
-      -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
-    },
-    config = function()
-      require('neo-tree').setup {
-        filesystem = {
-          filtered_items = {
-            hide_by_name = {
-              'node_modules',
-              'Vendors',
-              'vendors',
-              'vendor',
-              'Vendor',
-            },
-          },
-          buffers = {
-            follow_current_file = {
-              enabled = true, -- This will find and focus the file in the active buffer every time
-              --              -- the current file is changed while the tree is open.
+			-- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+		},
+		config = function()
+			require("neo-tree").setup({
+				filesystem = {
+					filtered_items = {
+						hide_by_name = {
+							"node_modules",
+							"Vendors",
+							"vendors",
+							"vendor",
+							"Vendor",
+						},
+					},
+					buffers = {
+						follow_current_file = {
+							enabled = true, -- This will find and focus the file in the active buffer every time
+							--              -- the current file is changed while the tree is open.
 
-              leave_dirs_open = false, -- `false` closes auto expanded dirs, such as with `:Neotree reveal`
-            },
-          },
-        },
-      }
-      vim.cmd [[nnoremap \ :Neotree toggle<cr>]]
-    end,
-  },
-  {
-    'alexghergh/nvim-tmux-navigation',
-    config = function()
-      local nvim_tmux_nav = require 'nvim-tmux-navigation'
+							leave_dirs_open = false, -- `false` closes auto expanded dirs, such as with `:Neotree reveal`
+						},
+					},
+				},
+			})
+			vim.cmd([[nnoremap \ :Neotree toggle<cr>]])
+		end,
+	},
+	{
+		"alexghergh/nvim-tmux-navigation",
+		config = function()
+			local nvim_tmux_nav = require("nvim-tmux-navigation")
 
-      nvim_tmux_nav.setup {
-        disable_when_zoomed = true, -- defaults to false
-      }
+			nvim_tmux_nav.setup({
+				disable_when_zoomed = true, -- defaults to false
+			})
 
-      vim.keymap.set('n', '<C-h>', nvim_tmux_nav.NvimTmuxNavigateLeft)
-      vim.keymap.set('n', '<C-j>', nvim_tmux_nav.NvimTmuxNavigateDown)
-      vim.keymap.set('n', '<C-k>', nvim_tmux_nav.NvimTmuxNavigateUp)
-      vim.keymap.set('n', '<C-l>', nvim_tmux_nav.NvimTmuxNavigateRight)
-      vim.keymap.set('n', '<C-\\>', nvim_tmux_nav.NvimTmuxNavigateLastActive)
-      vim.keymap.set('n', '<C-Space>', nvim_tmux_nav.NvimTmuxNavigateNext)
-    end,
-  },
+			vim.keymap.set("n", "<C-h>", nvim_tmux_nav.NvimTmuxNavigateLeft)
+			vim.keymap.set("n", "<C-j>", nvim_tmux_nav.NvimTmuxNavigateDown)
+			vim.keymap.set("n", "<C-k>", nvim_tmux_nav.NvimTmuxNavigateUp)
+			vim.keymap.set("n", "<C-l>", nvim_tmux_nav.NvimTmuxNavigateRight)
+			vim.keymap.set("n", "<C-\\>", nvim_tmux_nav.NvimTmuxNavigateLastActive)
+			vim.keymap.set("n", "<C-Space>", nvim_tmux_nav.NvimTmuxNavigateNext)
+		end,
+	},
+	{
+		"windwp/nvim-ts-autotag",
+		config = function()
+			local ts_autotag = require("nvim-ts-autotag")
+			ts_autotag.setup({
+				opts = {
+					enable_close = true,
+					enable_rename = true,
+				},
+			})
+		end,
+	},
+	{
+		"luckasRanarison/tailwind-tools.nvim",
+		name = "tailwind-tools",
+		build = ":UpdateRemotePlugins",
+		dependencies = {
+			"nvim-treesitter/nvim-treesitter",
+			"nvim-telescope/telescope.nvim", -- optional
+			"neovim/nvim-lspconfig", -- optional
+		},
+		opts = {}, -- your configuration
+	},
 }
-
--- zsh/.zshrc
+++ zsh/.zshrc
@@ -108,6 +108,18 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 export PATH=~/go/bin:$PATH
-export PYENV_ROOT="$HOME/.pyenv"
-[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
-eval "$(pyenv init -)"
+# export PYENV_ROOT="$HOME/.pyenv"
+# [[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
+# eval "$(pyenv init -)"
+
+# bun completions
+[ -s "/home/roosic/.bun/_bun" ] && source "/home/roosic/.bun/_bun"
+
+# bun
+export BUN_INSTALL="$HOME/.bun"
+export PATH="$BUN_INSTALL/bin:$PATH"
+export PODMAN_IGNORE_CGROUPSV1_WARNING=1
+export PATH=$HOME/.config/composer/vendor/bin:$PATH
+#THIS MUST BE AT THE END OF THE FILE FOR SDKMAN TO WORK!!!
+export SDKMAN_DIR="$HOME/.sdkman"
+[[ -s "$HOME/.sdkman/bin/sdkman-init.sh" ]] && source "$HOME/.sdkman/bin/sdkman-init.sh"
