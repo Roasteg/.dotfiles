c1525b48641e178b2d8f4f09d29c8515273c55dd
Roasteg <krostislav468@gmail.com>
2025-05-19 14:36:38 +0300

    fix tailwind lsp

--- nvim/.config/nvim/lua/plugins/lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/lspconfig.lua
@@ -1,59 +1,51 @@
 return {
-  "neovim/nvim-lspconfig",
-  ---@class PluginLspOpts
-  opts = {
-    ---@type lspconfig.options
-    servers = {
-      vtsls = {
-        tsserver = {
-          globalPlugins = {
-            {
-              name = "@vue/typescript-plugin",
-              location = LazyVim.get_pkg_path("vue-language-server", "/node_modules/@vue/language-server"),
-              languages = { "vue" },
-              configNamespace = "typescript",
-              enableForWorkspaceTypeScriptVersions = true,
+  {
+    "neovim/nvim-lspconfig",
+    ---@class PluginLspOpts
+    opts = {
+      ---@type lspconfig.options
+      servers = {
+        pyright = {},
+        eslint = {},
+        bashls = {},
+        sqlls = {},
+        cssls = {},
+        css_variables = {},
+        cssmodules_ls = {},
+        tailwindcss = {
+          filetypes = { "css", "scss", "vue", "html", "javascriptreact", "typescriptreact", "typescript", "javascript" },
+          settings = {
+            tailwindCSS = {
+              classAttributes = { "class", "ui" },
+              experimental = {
+                classRegex = { { "ui:\\s*{([^)]*)\\s*}", "(?:'|\"|`)([^']*)(?:'|\"|`)" } },
+              },
             },
           },
         },
-      },
-      pyright = {},
-      eslint = {},
-      bashls = {},
-      sqlls = {},
-      cssls = {},
-      css_variables = {},
-      cssmodules_ls = {},
-      tailwindcss = {},
-      volar = {
-        init_options = {
-          vue = {
-            hybridMode = true,
-          },
-        },
-      },
-      intelephense = {
-        settings = {
-          intelephense = {
-            format = {
-              enable = false,
+        intelephense = {
+          settings = {
+            intelephense = {
+              format = {
+                enable = false,
+              },
             },
           },
         },
-      },
-      phpactor = {
-        filetypes = { "php" },
-        setup = {
-          init_options = {
-            ["language_server_phpstan.enabled"] = false,
-            ["language_server_psalm.enabled"] = false,
-            ["indexer.exclude_patterns"] = {
-              "/vendor/**/Tests",
-              "/vendor/**/tests/**/*",
-              "/vendor/composer/**/*",
-              "/generated/**/*",
-              "/upload/**",
-              "/assets/**",
+        phpactor = {
+          filetypes = { "php" },
+          setup = {
+            init_options = {
+              ["language_server_phpstan.enabled"] = false,
+              ["language_server_psalm.enabled"] = false,
+              ["indexer.exclude_patterns"] = {
+                "/vendor/**/Tests",
+                "/vendor/**/tests/**/*",
+                "/vendor/composer/**/*",
+                "/generated/**/*",
+                "/upload/**",
+                "/assets/**",
+              },
             },
           },
         },
